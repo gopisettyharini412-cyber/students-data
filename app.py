46f614a3627f742ea160692bc35b878c1a55beb0
gopisettyharini412-cyber <gopisettyharini412@gmail.com>
2026-06-16 22:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ app = Flask(__name__)
 # DATABASE CONNECTION
 # =========================
 def get_db_connection():
-    return mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME"),
-        port=int(os.environ.get("DB_PORT", 3306))
-    )
+    try:
+        return mysql.connector.connect(
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME"),
+            port=int(os.environ.get("DB_PORT", 3306))
+        )
+    except Exception as e:
+        print("DB Connection Error:", e)
+        return None
 
 # =========================
 # HOME PAGE
